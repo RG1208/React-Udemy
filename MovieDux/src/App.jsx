305d3146768a2afd273eeba4f0c0200da2a6954d
RG1208 <rachitgarg1208@gmail.com>
2025-05-19 12:06:36 +0000

    React Componetns

---
+++ MovieDux/src/App.jsx
@@ -0,0 +1,18 @@
+import './styles.css'
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <header className="header">
+          <h1>Welcome to MovieDux</h1>
+        </header>
+        <footer className="footer">
+          <p className="footer"> Footer content here </p>
+        </footer>
+      </div>
+    </>
+  )
+}
+
+export default App
