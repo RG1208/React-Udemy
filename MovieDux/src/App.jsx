3fb9a50b27b76cd5d9b19334b8fe8a4b0a529368
RG1208 <rachitgarg1208@gmail.com>
2025-05-19 14:31:08 +0000

    feat: Movies Grid

--- MovieDux/src/App.jsx
+++ MovieDux/src/App.jsx
@@ -1,18 +1,21 @@
+import "./App.css"
 import './styles.css'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MoviesGrid from "./components/MoviesGrid";
+
 function App() {
 
   return (
-    <>
       <div className="App">
-        <header className="header">
-          <h1>Welcome to MovieDux</h1>
-        </header>
-        <footer className="footer">
-          <p className="footer"> Footer content here </p>
-        </footer>
-      </div>
-    </>
-  )
+        <div className="container">
+          <Header></Header>
+          <MoviesGrid></MoviesGrid>
+        </div>
+
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default App
